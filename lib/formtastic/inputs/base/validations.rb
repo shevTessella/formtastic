df38f4281c3f588442f47674d0ad858dcb9e7536
Victor Shen <victor.shen@tessella.com>
2018-05-18 12:34:57 -0400

    initalize instance variable

--- lib/formtastic/inputs/base/validations.rb
+++ lib/formtastic/inputs/base/validations.rb
@@ -163,7 +163,7 @@ module Formtastic
         end
 
         def not_required_through_negated_validation?
-          @not_required_through_negated_validation
+          @not_required_through_negated_validation ||= nil
         end
 
         def not_required_through_negated_validation!
